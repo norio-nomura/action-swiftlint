c875a47f86683769e8e3dd466b3e0a14f1d81934
marsha97 <angelamdm97@gmail.com>
2020-01-10 12:37:09 +0700

    config_params

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-set -o pipefail && swiftlint "$@" | stripPWD | convertToGitHubActionsLoggingCommands
+set -o pipefail && swiftlint lint "$@" --config .github/.swiftlint.yml | stripPWD | convertToGitHubActionsLoggingCommands
