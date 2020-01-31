0bf8f5aeed4c05d3c152044867d3a0a78ee269f0
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-01-31 10:13:08 +0100

    Add PROJECT_PATH environment
    + replace tabs with whitespaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,20 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
+if ! ${PROJECT_PATH+false};
+then
+   cd ${PROJECT_PATH}
+fi
+
 if ! ${DIFF_BASE+false};
 then
-	changedFiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+    changedFiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
 
-	if [ -z "$changedFiles" ]
-	then
-		echo "No Swift file changed"
-		exit
-	fi
+    if [ -z "$changedFiles" ]
+    then
+        echo "No Swift file changed"
+        exit
+    fi
 fi
 
 set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
